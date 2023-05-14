409624207d598a3f4a479ef1f111247da33a8893
Thatchaphol <Thatchaphol.po@ku.th>
2023-05-14 19:53:18 +0700

    Ionic

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  
 })
 export class Tab1Page {
 
-  constructor() {}
+  weight!: number;
+  height!: number;
+  bmi: number = 0;
+  bmiString!: string;
+  public alertButtons = ['OK'];
 
-}
+  constructor(private router: Router) {}
+
+  bmiCal(){
+    this.height = this.height/100;
+    this.bmi = this.weight / (this.height*this.height);
+    if(this.bmi < 18.5){
+      this.bmiString = "UnderWeight"
+    }
+    else if(this.bmi > 18.5 && this.bmi < 22.90){
+      this.bmiString = "Normal Weight"
+    }
+    else if(this.bmi > 23 && this.bmi < 24.90){
+      this.bmiString = "Obesity degree 1"
+    }
+    else if(this.bmi < 25 && this.bmi < 29.90){
+      this.bmiString = "Obesity degree 2"
+    }
+    else if(this.bmi > 30){
+      this.bmiString = "Obesity degree 3"
+    }
+  }
+
+
+} 
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { maleOutline, femaleOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,6 +8,26 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  sex: string = "male";
+  weight!: number;
+  height!: number;
+  age!: number;
+  bmr: number = 0;
+  public alertButtonsBmr = ['OK'];
 
+  constructor() {
+    //addIcons({maleOutline, femaleOutline});
+  }
+
+  bmrCal() {
+
+    if(this.sex == "male"){
+      this.bmr = (10*this.weight)+(6.25*this.height)-(5*this.age)+5
+    }
+    else if(this.sex == "female"){
+      this.bmr = (10*this.weight)+(6.25*this.height)-(5*this.age)-161
+    }
+
+  }
+  
 }
